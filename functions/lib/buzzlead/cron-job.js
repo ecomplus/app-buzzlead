7ee2f1f37a7e85ba39285c4699d49ca3a3417400
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-06-21 07:15:36 -0300

    chore: set final options to app

---
+++ functions/lib/buzzlead/cron-job.js
@@ -0,0 +1,99 @@
+// DO NOT COPY TO v2
+const logger = require('firebase-functions/logger')
+const { getFirestore } = require('firebase-admin/firestore')
+const getAppData = require('./../../lib/store-api/get-app-data')
+const updateAppData = require('../store-api/update-app-data')
+
+module.exports = async ({ appSdk }) => {
+  const d = new Date()
+  d.setDate(d.getDate() - 3)
+  const storeId = 1032
+  const endpoint = '/orders.json' +
+    '?financial_status.current=paid' +
+    `&financial_status.updated_at>=${d.toISOString()}` +
+    `&fulfillments.flags!=from-tiny` +
+    `&updated_at<=${(new Date(Date.now() - 1000 * 60 * 5).toISOString())}` +
+    '&fields=_id,number,financial_status.current' +
+    '&sort=created_at'
+  const { response } = await appSdk.apiRequest(storeId, endpoint, 'GET')
+  const { data: { result } } = response
+  const db = getFirestore()
+  const ordersToQueue = []
+  for (let i = 0; i < result.length; i++) {
+    const order = result[i]
+    const doc = await db.doc(`exported_orders/${order._id}`).get()
+    if (!doc.exists) {
+      ordersToQueue.push(order)
+    }
+  }
+  if (ordersToQueue.length) {
+    const appData = await getAppData({ appSdk, storeId })
+    const action = 'exportation'
+    const queue = 'order_ids'
+    let queueList = appData[action] && appData[action][queue]
+    if (!Array.isArray(queueList)) {
+      queueList = []
+    }
+    ordersToQueue.forEach(({ _id: nextId }) => {
+      if (!queueList.includes(nextId)) {
+        queueList.unshift(nextId)
+        logger.debug(`> add to queue ${nextId}`)
+      }
+    })
+    await updateAppData({ appSdk, storeId }, {
+      [action]: {
+        ...appData[action],
+        [queue]: queueList
+      }
+    })
+  }
+  logger.info(`${ordersToQueue.length} orders to queue`, { ordersToQueue })
+}
+
+
+
+const getAppData = require('./../../lib/store-api/get-app-data')
+const updateAppData = require('../store-api/update-app-data')
+
+module.exports = async ({ appSdk }) => {
+  const d = new Date()
+  d.setDate(d.getDate() - 3)
+  const storeId = 1032
+  const endpoint = '/orders.json' +
+    '?financial_status.current=paid' +
+    `&financial_status.updated_at>=${d.toISOString()}` +
+    `&updated_at<=${(new Date(Date.now() - 1000 * 60 * 5).toISOString())}` +
+    '&fields=_id,number' +
+    '&sort=created_at'
+  const { response } = await appSdk.apiRequest(storeId, endpoint, 'GET')
+  const { data: { result } } = response
+  console.log('Pedidos buscados', JSON.stringify(result))
+  const ordersToQueue = []
+  for (let i = 0; i < result.length; i++) {
+    const orderId = result[i]._id
+    ordersToQueue.push(orderId)
+  }
+  console.log('Antes da fila', ordersToQueue.length)
+  if (ordersToQueue.length) {
+    const appData = await getAppData({ appSdk, storeId })
+    const action = 'exportation'
+    const queue = 'order_ids'
+    let queueList = appData[action] && appData[action][queue]
+    if (!Array.isArray(queueList)) {
+      queueList = []
+    }
+    ordersToQueue.forEach((nextId) => {
+      if (!queueList.includes(nextId)) {
+        queueList.unshift(nextId)
+      }
+    })
+  console.log('Já com a fila', queueList.length)
+    await updateAppData({ appSdk, storeId }, {
+      [action]: {
+        ...appData[action],
+        [queue]: queueList
+      }
+    })
+  }
+  console.log(`${ordersToQueue.length} orders to queue`, { ordersToQueue })
+}
--- functions/routes/ecom/webhook.js
+++ functions/routes/ecom/webhook.js
@@ -1,3 +1,8 @@
+const axios = require('axios')
+const { firestore } = require('firebase-admin')
+const sendConversion = require('../../lib/buzzlead/send-conversions')
+const updateConversion = require('../../lib/buzzlead/update-conversions')
+
 // read configured E-Com Plus app data
 const getAppData = require('./../../lib/store-api/get-app-data')
 
@@ -31,26 +36,120 @@ exports.post = ({ appSdk }, req, res) => {
       }
 
       /* DO YOUR CUSTOM STUFF HERE */
-      const { token, apikey } = appData
-
-      if (!token && !apikey) {
-        res.status(401).send('App data doesnt contain token or apikey to authenticate')
+      const { resource } = trigger
+      let docId, manualQueue, isCart, isCustomer
+      const updateManualQueue = () => {
+        if (manualQueue) {
+          const data = { manual_queue: manualQueue }
+          setTimeout(() => {
+            appSdk.apiApp(storeId, 'data', 'PATCH', data).catch(console.error)
+          }, 1000)
+          manualQueue = null
+        }
       }
+      if (resource === 'applications') {
+        const { body } = trigger
+        if (body && Array.isArray(body.manual_queue) && body.manual_queue.length) {
+          manualQueue = body.manual_queue
+          const nextId = manualQueue[0]
+          if (typeof nextId === 'string' && /[a-f0-9]{24}/.test(nextId)) {
+            docId = nextId.trim()
+          }
+          manualQueue.shift()
+        }
+      } else if (trigger.action !== 'delete') {
+        docId = trigger.resource_id || trigger.inserted_id
+        isCart = resource === 'carts'
+        isCustomer = resource === 'customers'
+      }
+      if (docId) {
+        const docEndpoint = `orders/${docId}.json`
+        return appSdk.apiRequest(storeId, docEndpoint).then(async ({ response }) => {
+          const doc = response.data
+          const urls = []
+          const webhooksPromises = []
+          const addWebhook = (options) => {
+            const url = options && options.webhook_uri
+            if (url && !urls.includes(url) && ((!isCart || options.send_carts) && (!isCustomer || options.send_customers))) {
+              urls.push(url)
+              console.log(`Trigger for Store #${storeId} ${docEndpoint} => ${url}`)
+              if (
+                options.skip_pending === true &&
+                (!doc.financial_status || doc.financial_status.current === 'pending')
+              ) {
+                return null
+              }
+              console.log(`> Sending ${resource} notification`)
+              const token = options.webhook_token
+              const headerProp = options.webhook_prop_token
+              let headers
+              if (token && !headerProp) {
+                headers = {
+                  'Authorization': `Bearer ${token}`
+                }
+              } else if (token && headerProp) {
+                headers = {}
+                headers[headerProp] = token
+              }
 
-      // all done
-      res.send(ECHO_SUCCESS)
+              let body = {
+                storeId,
+                trigger,
+                [isCart ? 'cart' : isCustomer ? 'customer' : 'order']: doc
+              }
+
+              webhooksPromises.push(
+                axios({
+                  method: 'post',
+                  url,
+                  headers,
+                  data: body
+                })
+                .then(({ status }) => {
+                  updateManualQueue()
+                  console.log(`> ${status}`)
+                })
+                .catch(error => {
+                  if (error.response && error.config) {
+                    const err = new Error(`#${storeId} ${docId} POST to ${error.config.url} failed`)
+                    const { status, data } = error.response
+                    err.response = {
+                      status,
+                      data: JSON.stringify(data)
+                    }
+                    err.data = JSON.stringify(error.config.data)
+                    return console.error(err)
+                  }
+                  console.error(error)
+                })
+              )
+            }
+          }
+          const { webhooks } = appData
+          if (Array.isArray(webhooks)) {
+            webhooks.forEach(addWebhook)
+          }
+          addWebhook(appData)
+          return Promise.all(webhooksPromises).then(() => {
+            if (!res.headersSent) {
+              return res.sendStatus(200)
+            }
+          })
+        }).catch(error => {
+          console.error(error)
+          const status = error.response
+            ? error.response.status || 500 : 409
+          return res.sendStatus(status)
+        })
+      }
+      updateManualQueue()
+      res.sendStatus(204)
     })
 
     .catch(err => {
       if (err.name === SKIP_TRIGGER_NAME) {
         // trigger ignored by app configuration
         res.send(ECHO_SKIP)
-      } else if (err.appWithoutAuth === true) {
-        const msg = `Webhook for ${storeId} unhandled with no authentication found`
-        const error = new Error(msg)
-        error.trigger = JSON.stringify(trigger)
-        console.error(error)
-        res.status(412).send(msg)
       } else {
         // console.error(err)
         // request to Store API with error response
